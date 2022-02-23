d25c50c38f84449cacc581424a9595be9c83467e
viddushiva <viddushiva1@gmail.com>
2022-02-23 18:20:46 +0530

    s

--- src/App.js
+++ src/App.js
@@ -6,20 +6,35 @@ import Functi from './Components/Functi';
 // import {Functi,Sum,Sub,Mul} from './Components/Functi';
 // import Second from './Components/Second';
 class App extends Component{
+ 
 state={
   displayFunction:false,
   displayClass:false
 }
+displayFunctio=()=>{
+  this.setState({
+    displayFunction:!this.state.displayFunction
+  })
+}
+displayClass=()=>{
+  this.setState(
+    {
+      displayClass:!this.state.displayClass
+    }
+  )
+}
 render(){
+  
   return (
     <div className="App">
         <h1 className='one'> Styling using functional and class component</h1>
 
    
-     <button className="first" onClick={()=>this.setState({displayFunction:!this.state.displayFunction})}>to see styling in functional component</button>
-     <button className="biu" onClick={()=>this.setState({displayClass:!this.state.displayClass})} >to see styling in class component</button>
-     {this.state.displayFunction?<Functi/>:null}
-     {this.state.displayClass?<Clas/>:null}
+     <button className="first" onClick={this.displayFunctio}>to see styling in functional component</button>
+
+     <button className="biu" onClick={this.displayClass} >to see styling in class component</button>
+     {this.state.displayFunction?<Functi/>:<div>there is no functional component</div>}
+     {this.state.displayClass?<Clas/>:<div>there is no class component</div>}
 
 
 
